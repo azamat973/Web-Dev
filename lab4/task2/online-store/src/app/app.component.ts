9d2a5bbed9716df6e6798a8909983e51d723357b
azamat <az_alipbek@kbtu.kz>
2026-02-14 22:48:28 +0500

    lab4

--- lab4/task2/online-store/src/app/app.component.ts
+++ lab4/task2/online-store/src/app/app.component.ts
@@ -5,10 +5,11 @@ import { ProductListComponent } from './components/product-list/product-list.com
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, ProductListComponent], // RouterOutlet-ті алып тастадық
+  imports: [CommonModule, ProductListComponent], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'online-store';
+
 }
