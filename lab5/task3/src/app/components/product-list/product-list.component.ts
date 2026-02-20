08c27b21b73b5005ede3c19fce8b98b212f8f615
azamat <az_alipbek@kbtu.kz>
2026-02-20 13:11:53 +0500

    lab5

---
+++ lab5/task3/src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductItemComponent } from '../product-item/product-item.component';
+import { Product } from '../../shared/models/product.model';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [CommonModule, ProductItemComponent],
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  @Input() products: Product[] = [];
+  @Input() selectedCategoryName: string = '';
+
+  onDeleteProduct(productId: number): void {
+    const index = this.products.findIndex(p => p.id === productId);
+    if (index !== -1) {
+      this.products.splice(index, 1);
+    }
+  }
+
+  onLikeProduct(productId: number): void {
+    const product = this.products.find(p => p.id === productId);
+    if (product) {
+      product.likes += 1;
+    }
+  }
+}
