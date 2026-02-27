982cb37bb9d08bbdeb01d2efbd4ddb12dc9b56ef
azamat <az_alipbek@kbtu.kz>
2026-02-27 14:04:07 +0500

    lab6

---
+++ lab6/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class App {
+  protected readonly title = signal('my_album');
+}
