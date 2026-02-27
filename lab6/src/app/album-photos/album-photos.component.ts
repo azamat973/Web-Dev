5780b4426ac5489a53c0cdb291ab51228defe4b5
azamat <az_alipbek@kbtu.kz>
2026-02-27 14:00:46 +0500

    lab6

---
+++ lab6/src/app/album-photos/album-photos.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { AlbumService } from '../services/album.service';
+import { Photo } from '../models/photo';
+
+@Component({
+  selector: 'app-album-photos',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './album-photos.component.html',
+  styleUrls: ['./album-photos.component.css']
+})
+export class AlbumPhotosComponent implements OnInit {
+  photos: Photo[] = [];
+  loading = true;
+  albumId!: number;
+
+  constructor(
+    private route: ActivatedRoute,
+    private albumService: AlbumService
+  ) {}
+
+  ngOnInit(): void {
+    this.albumId = Number(this.route.snapshot.paramMap.get('id'));
+    this.albumService.getAlbumPhotos(this.albumId).subscribe({
+      next: (data) => {
+        this.photos = data;
+        this.loading = false;
+      },
+      error: () => this.loading = false
+    });
+  }
+}
